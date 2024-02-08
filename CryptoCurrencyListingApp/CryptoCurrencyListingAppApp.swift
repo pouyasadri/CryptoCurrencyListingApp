4d3513f9e4bdeb12ddd2286b91d018e99043e26c
pouyasadri <pouyapaghandeh78@gmail.com>
2024-02-08 14:45:13 +0100

    Initial Commit

---
+++ CryptoCurrencyListingApp/CryptoCurrencyListingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyListingAppApp.swift
+//  CryptoCurrencyListingApp
+//
+//  Created by Pouya Sadri on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyListingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
